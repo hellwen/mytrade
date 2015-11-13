fbdcebb05511871c1fa00cc6bb45c737393e6cb3
Hellwen wu <hellwen.wu@gmail.com>
2015-11-13 16:50:21 +0800

    ok

--- mytrade/setup/forms.py
+++ mytrade/setup/forms.py
@@ -1,42 +1,22 @@
 # -*- coding: utf-8 -*-
 from flask_wtf import Form
-from wtforms import TextField, PasswordField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms import TextField, BooleanField
+from wtforms.validators import DataRequired, Length
 
-from .models import Unit, Item_Group, Company
+from .models import Unit#, Item_Group, Company
 
 class UnitForm(Form):
     unit_name = TextField('Unit Name',
                     validators=[DataRequired(), Length(max=80)])
-    must_be_whole_number = Bollean('Must be whole Number',
-                    validators=[DataRequired(), default=False])
+    must_be_whole_number = BooleanField('Must be whole Number',
+                    default="checked")
 
     def __init__(self, *args, **kwargs):
         super(UnitForm, self).__init__(*args, **kwargs)
         self.unit = None
 
     def validate(self):
-        initial_validation = super(RegisterForm, self).validate()
-        if not initial_validation:
-            return False
-        unit = Unit.query.filter_by(unit_name=self.unit_name.data).first()
-        if unit:
-            self.unit_name.errors.append("Unit Name already registered")
-            return False
-        return True
-
-class ItemGroupForm(Form):
-    unit_name = TextField('Unit Name',
-                    validators=[DataRequired(), Length(max=80)])
-    must_be_whole_number = Bollean('Must be whole Number',
-                    validators=[DataRequired(), default=False])
-
-    def __init__(self, *args, **kwargs):
-        super(UnitForm, self).__init__(*args, **kwargs)
-        self.unit = None
-
-    def validate(self):
-        initial_validation = super(RegisterForm, self).validate()
+        initial_validation = super(UnitForm, self).validate()
         if not initial_validation:
             return False
         unit = Unit.query.filter_by(unit_name=self.unit_name.data).first()
--- mytrade/setup/views.py
+++ mytrade/setup/views.py
@@ -1,12 +1,86 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template
+from flask import (
+    Blueprint,
+    render_template,
+    request,
+    flash,
+    url_for,
+    redirect,
+)
 from flask_login import login_required
 
-blueprint = Blueprint("user", __name__, url_prefix='/users',
+from .models import (
+    Unit,
+    #Item_Group,
+    #Company,
+)
+from .forms import (
+    UnitForm,
+)
+
+
+setup = Blueprint("setup", __name__, url_prefix='/setup',
                       static_folder="../static")
 
 
-@blueprint.route("/")
+@setup.route("/")
 @login_required
-def members():
-    return render_template("users/members.html")
+def setup():
+    return render_template("user/members.html")
+
+@setup.route('/units', methods=['GET'])
+@login_required
+def units():
+    """Unit Query"""
+    units = Unit.query.all()
+    return render_template('units.html', units=units)
+
+@setup.route('/unit_add', methods=['POST'])
+@login_required
+def unit_add():
+    """Unit Add"""
+    return_url = request.args.get('next', url_for(".units"))
+    unitform = UnitForm(Unit)
+    if unitform.validate_on_submit():
+        if '_add_another' in request.form:
+            flash('Created successfully.')
+            return redirect(url_for('.unit_add', url=return_url))
+        else:
+            return redirect(return_url)
+
+    return render_template('unit_add.html', unitform=unitform,
+        return_url=return_url)
+
+@setup.route('/unit_edit/<unit_id>', methods=['GET', 'POST'])
+@login_required
+def unit_edit(unit_id):
+    """Unit Edit"""
+    return_url = request.args.get('next', url_for(".units"))
+    unit = Unit.get_one(unit_id)
+
+    if unit is None:
+        return redirect(return_url)
+
+    unitform = UnitForm(unit)
+    if unitform.validate_on_submit():
+        try:
+            unitform.populate_obj(unit)
+            unit.commit()
+            return True
+        except Exception:
+            flash('Failed to update model.')
+            return False
+
+    return render_template('unit_edit.html', unitform=unitform,
+        return_url=return_url)
+
+@setup.route('/unit_delete/<unit_id>', methods=['POST'])
+@login_required
+def unit_delete(unit_id):
+    """Unit Delete"""
+    return_url = request.args.get('next', url_for(".units"))
+    unit = Unit.get_one(unit_id)
+
+    if unit:
+        unit.delete()
+    return redirect(return_url)
