132956ade07ded6cd31d9dc9fa13702a5ef8f5d4
Hellwen wu <hellwen.wu@gmail.com>
2015-12-02 15:14:46 +0800

    ok

--- mytrade/settings.py
+++ mytrade/settings.py
@@ -16,7 +16,6 @@ class ProdConfig(Config):
     """Production configuration."""
     ENV = 'prod'
     DEBUG = False
-    #SQLALCHEMY_DATABASE_URI = 'postgres://dahebaaijmvoit:SuW5nMaqqN-qZ-RSjLtVBmJpEC@ec2-54-225-192-128.compute-1.amazonaws.com:5432/dfu098bqnu8kig'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '')
     DEBUG_TB_ENABLED = False
 
